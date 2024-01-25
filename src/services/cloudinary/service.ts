47c2be5787e6245981b6b75497bbdf776d31b799
Adebiyi Blessing <yoyoplenty@gmail.com>
2024-01-25 05:51:21 +0100

    Property upload implementation

--- src/services/cloudinary/service.ts
+++ src/services/cloudinary/service.ts
@@ -25,7 +25,6 @@ export class CloudinaryService {
           console.error(error);
           reject(error);
         } else {
-          console.log(result);
           resolve(result);
         }
       });
